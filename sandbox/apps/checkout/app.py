fafcb482feb105c323bd4012c174ea2a2d8591a1
Tomasz Zdanowski <tomasz@enduhub.com>
2017-05-29 19:39:05 +0000

    added translations and deleted sandbox

--- sandbox/apps/checkout/app.py
+++
@@ -1,8 +0,0 @@
-from oscar.apps.checkout.app import CheckoutApplication
-from apps.checkout import views
-
-class OverriddenCheckoutApplication(CheckoutApplication):
-    # Specify new view for payment details
-    payment_details_view = views.EnduPaymentDetailsView
-
-application = OverriddenCheckoutApplication()
